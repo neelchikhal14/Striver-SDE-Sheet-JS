f825de3ec25dce00200a79b3e7efca6fe2b464e3
neelchikhal14 <neelchikhal14@gmail.com>
2023-05-09 23:49:51 +0100

    completed 7-1-2

---
+++ Recursion/step-7-1-2/index.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+
+var myPow = function (x, n) {
+  let tempN = n,
+    result = 1;
+
+  if (n < 0) {
+    tempN = tempN * -1;
+  }
+
+  while (tempN > 0) {
+    // odd power
+    if (tempN % 2 === 1) {
+      result = result * x;
+      tempN = tempN - 1;
+    } else {
+      x = x * x;
+      tempN /= 2;
+    }
+  }
+
+  if (n < 0) {
+    return 1 / result;
+  } else return result;
+};
+
+console.log(myPow(2.0, -2));
